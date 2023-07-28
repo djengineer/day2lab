d86c3643fd83a23d452f4e3d869a1b5e954c4724
djengineer <djengineer9@gmail.com>
2023-07-28 22:12:08 +0800

    Update mutillidae_start.sh

--- lab_servers/mutillidae_start.sh
+++ lab_servers/mutillidae_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose -f ./mutillidae-docker/docker-compose.yml up --build -d
+#docker-compose -f ./mutillidae-docker/docker-compose.yml up --build -d
+docker-compose -f ./mutillidae-docker/docker-compose.yml up --build 
